a1261d4437aab45e6d68cc05ddb9efbdb1ee1c3d
RohiniChougale <102050101+RohiniChougale@users.noreply.github.com>
2022-03-26 14:53:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "web" {
   count = "2"
   ami  = "ami-04893cdb768d0f9ee"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-09afd9287225e7a02"
   key_name      = "linux-demo"
   tags = {
     Name = "Terraform_Demo"
