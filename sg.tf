b388327f7a410f36824210931ce90179762cf678
RohiniChougale <102050101+RohiniChougale@users.noreply.github.com>
2022-03-26 14:54:22 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,20 +1,32 @@
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http" 
-  description = "Allow HTTP inbound traffic from LB"
+resource "aws_security_group" "b" {
+  name        = "b"
+  description = "Allow http inbound traffic"
 
   ingress {
-    #http
+    description = "http from VPC"
     from_port   = 80
     to_port     = 80
-    protocol    = "TCP"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "allow http inbound"
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+
+    ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "allow all traffic outbound"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "main"
   }
 }
