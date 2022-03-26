6072244d49b391a6630c0e06c193bf75fdb72dd8
RohiniChougale <102050101+RohiniChougale@users.noreply.github.com>
2022-03-26 14:16:48 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_http_asg" {
-  name        = var.asg_security_group_name
+  name        = allow_http_asg
   description = "Allow HTTP inbound traffic from LB"
 
   ingress {
